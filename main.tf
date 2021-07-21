b92c3ca28340a62fa2d2852e94332f9d901748e8
Nemo <me@captnemo.in>
2021-07-22 01:01:54 +0530

    Add playtube v1

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "basic-test" {
   source = "git::https://github.com/captn3m0/terraform-data-looker-ips?ref=v2.0.0"
 }
+
+module "playtube" {
+  source = "git::https://github.com/captn3m0/playtube?ref=v1.0.0"
+}
