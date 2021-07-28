96426f8751591da520e3a845b20b4f863b103e59
Nemo <commits@captnemo.in>
2021-07-28 15:21:50 +0530

    test for spring releases

---
+++ spring/main.tf
@@ -0,0 +1,7 @@
+module "a" {
+  source = "git::https://github.com/spring-projects/spring-framework?ref=v5.3"
+}
+
+module "b" {
+  source = "git::https://github.com/spring-projects/spring-framework?ref=v5.2"
+}
